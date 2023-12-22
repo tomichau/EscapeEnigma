5030c14c46ee11463d27672845b6d1f50e581408
tomichau <56234855+tomichau@users.noreply.github.com>
2023-12-22 15:04:54 +0100

    Add files via upload

--- ascii design/Test.java
+++ ascii design/Test.java
@@ -1,3 +1,6 @@
+import extensions.File;
+import extensions.CSVFile;
+
 class Test extends Program{
     boolean quitter = false;
     String[] sauvegardes = new String[]{"Liste Sauvegardes :"};
@@ -16,6 +19,11 @@ class Test extends Program{
         while(!quitter){
             menu();
             debut();
+            salleActuel();
+            println("Après avoir écouté ce mystérieux fantôme vous expliquer comment faire pour s'échapper de cet endroit,");
+            println("vous et vous amis décidaient de vous diriger vers la seul porte disponnible.");
+            println("Vous entrez dans une sombre pièce, et au bout de quelques secondes, les lumières s'allument et un");
+            println("une personne s'y tiens en son centre.");
             salleActuel();
         }
         boolean sauvegardePartie = false;
@@ -85,7 +93,6 @@ class Test extends Program{
             println("et non je suis gentil en faite ahahah");
             delay(2000);
             clearScreen();
-            salleActuel();
         }
     }
 
@@ -106,18 +113,56 @@ class Test extends Program{
     void hall(){
         // Faire le texte d'entre dans le manoir (passage qui se referme);
         println(fantomeGentil);
-        println("Vous êtes maintenant enfermé dans ce manoir et votre seul moyen d'en sortir est de réussir toute les épreuves 
-                qui s'y trouvent, chaque salle sera sur le thème d'une matière scolaire ! Je sens que vous êtes plutôt doué donc vous 
-                devrier y arriver assez facilement non ?");
+        println("Vous êtes maintenant enfermé dans ce manoir et votre seul moyen d'en sortir est de réussir toute les épreuves");
+        println("qui s'y trouvent, chaque salle sera sur le thème d'une matière scolaire ! Je sens que vous êtes plutôt doué donc vous");
+        println("devrier y arriver assez facilement non ?");
         progression += 1;
+        clearScreen();
     }
 
     void anglais(){
         println(fileAsString("./image/monsieurGentil.txt"));
         println("Bienvenue dans la première salle de ce magnifique manoir !");
-        println("Comme tu le sais, chaque salle représente une matière, et celle-ci représente l'anglais !");
-
+        println("Comme tu le sais déjà, chaque salle représente une matière, et celle-ci représente l'anglais !");
+        println("Et c'est moi qui superviserai cette première épreuve. Commençons directement si tu es prêt");
+        String[][] questions = creationTableQuestion("./question/anglais.csv");
+        int points = 0;
+        String question = "";
+        String reponse = "";
+        println("Vous devez traduire les mots donnés en Anglais !");
+        println("N'oubliez pas de mettre une majuscule au début de votre mot et de ne pas mettre d'espace !");
+        for(int i = 0; i < length(questions); i += 1){
+            question = questions[i][0];
+            reponse = questions[i][1];
+            println("Traduisez : " + question + " : ");
+            String reponseJoueur = readString();
+            if(equals(reponseJoueur, reponse)){
+                println("Bonne réponse, +1 point !");
+                points += 1;
+            }else{
+                println("Aie... Mauvaise réponse, la réponse était : " + reponse);
+            }
+        }
+        if(points >= 15){
+            println("Félicitations, vous avez réussi avec un total de " + points + " points !");
+        }else{
+            println("Dommage, il vous fallait au minimum 15 points pour réussir et vous en avez eu : " + points);
+        }
     }
+
+    String[][] creationTableQuestion(String cheminFichier){
+        CSVFile table = loadCSV(cheminFichier);
+        int nombreLigne = rowCount(table);
+        int nombreColonne = columnCount(table);
+        String[][] contenuTable = new String[nombreLigne][nombreColonne];
+        for(int idxLigne = 0; idxLigne < nombreLigne; idxLigne += 1){
+            for(int idxColonne = 0; idxColonne < nombreColonne; idxColonne += 1){
+                contenuTable[idxLigne][idxColonne] = getCell(table, idxLigne, idxColonne);
+            }
+        }
+        return contenuTable;
+    }
+
 
     void sciences(){
         println("coucou");
@@ -130,4 +175,5 @@ class Test extends Program{
     void theEnd(){
         println("coucou");
     } 
+
 }
